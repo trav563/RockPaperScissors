3a0d8cc5bf5599611a2000b3445fe5b74d7d5928
Trav563 <trav563@gmail.com>
2023-02-28 14:31:44 -0500

    add playRound function and game function to keep score of rounds

--- javascript.js
+++ javascript.js
@@ -2,7 +2,54 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)]
-    console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
+function decideWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "tie"
+    } else if(
+        playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "scissors" && computerSelection == "paper" ||
+        playerSelection == "paper" && computerSelection == "rock"){
+            return "player";
+        } else {
+            return "computer";
+        }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = decideWinner(playerSelection, computerSelection);
+        if(result == "tie"){
+            return "It is a tie."
+        } else if(result == "player"){
+            return `You win! ${playerSelection} beats ${computerSelection}!`
+        } else {
+            return `You lose. ${computerSelection} beats ${playerSelection}`
+        }
+}
+
+function game(){
+let scorePlayer = 0;
+let scoreComputer = 0;
+console.log("Let's Begin");
+
+for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    if(decideWinner(playerSelection, computerSelection) == "player"){
+        scorePlayer++
+    } else if(decideWinner(playerSelection, computerSelection) == "computer"){
+        scoreComputer++
+    }
+}   if(scorePlayer > scoreComputer){
+    console.log("Player Wins!")
+}   else if(scoreComputer > scorePlayer){
+    console.log("Computer Wins")
+}   else {
+    console.log("We have a tie")
+}
+}
+
+game()
